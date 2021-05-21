43df13b6457b560e41e034c22e20474b04af41d9
Mark-James M <contact@markjames.dev>
2021-05-21 17:41:55 -0400

    - Working on linescore model.

---
+++ src/Models/Linescore.cs
@@ -0,0 +1,35 @@
+﻿namespace BaseballSharp.Models
+{
+    class Linescore
+    {
+        /// <summary>
+        /// The current inning of play.
+        /// </summary>
+        public int? CurrentInning { get; protected set; }
+
+        /// <summary>
+        /// Whether or not its the bottom or top of the inning.
+        /// </summary>
+        public string? InningHalf { get; protected set; }
+
+        /// <summary>
+        /// The number of innings scheduled for the game.
+        /// </summary>
+        public int? ScheduledInnings { get; protected set; }
+
+        /// <summary>
+        /// The number of runs for the home team
+        /// </summary>
+        public int? HometeamRuns { get; protected set; }
+
+        /// <summary>
+        /// The number of hits for the hometeam
+        /// </summary>
+        public int? HometeamHits { get; protected set; }
+
+        /// <summary>
+        /// The number of errors for the home team
+        /// </summary>
+        public int? HometeamErrors { get; protected set; }
+    }
+}
