aa0c473da80d922c39b98f257d5d696c64fb4918
Ken MacNeal <kenneth.b.macneal@gmail.com>
2021-06-28 11:22:37 -0500

    removing contructor and moving to assignment syntax to allow for easier update

--- src/Models/TeamRoster.cs
+++ src/Models/TeamRoster.cs
@@ -5,57 +5,40 @@
         /// <summary>
         /// The player's ID number. Useful for building other calls."
         /// </summary>
-        public int? PlayerId { get; protected set; }
+        public int? PlayerId { get; set; }
 
         /// <summary>
         /// The player's full name. eg: "Mike Trout"
         /// </summary>
-        public string? PlayerFullName { get; protected set; }
+        public string? PlayerFullName { get; set; }
 
         /// <summary>
         /// The player's position name (eg: "Pitcher").
         /// </summary>
-        public string? PlayerPosition { get; protected set; }
+        public string? PlayerPosition { get; set; }
 
         /// <summary>
         /// The player type (eg: "Pitcher").
         /// </summary>
-        public string? PlayerType { get; protected set; }
+        public string? PlayerType { get; set; }
 
         /// <summary>
         /// The code for the position (eg: 1).
         /// </summary>
-        public string? PositionCode { get; protected set; }
+        public string? PositionCode { get; set; }
 
         /// <summary>
         /// The team's ID number. Useful for building other calls."
         /// </summary>
-        public int? TeamId { get; protected set; }
+        public int? TeamId { get; set; }
 
         /// <summary>
         /// Abbreviated form of the positin (eg: "P" for Pitcher).
         /// </summary>
-        public string? PositionAbbreviation { get; protected set; }
+        public string? PositionAbbreviation { get; set; }
 
-        public string? StatusCode { get; protected set; }
+        public string? StatusCode { get; set; }
 
-        public string? StatusDescription { get; protected set; }
-
-
-        public TeamRoster(int? playerId, string? playerFullName, 
-            string? playerPosition, string? playerType, string? positionCode, 
-            int? teamId, string? positionAbbreviation, 
-            string? statusCode, string statusDescription)
-        {
-            PlayerId = playerId;
-            PlayerFullName = playerFullName;
-            PlayerPosition = playerPosition;
-            PlayerType = playerType;
-            PositionCode = positionCode;
-            TeamId = teamId;
-            PositionAbbreviation = positionAbbreviation;
-            StatusCode = statusCode;
-            StatusDescription = statusDescription;
-        }
+        public string? StatusDescription { get; set; }
     }
 }
