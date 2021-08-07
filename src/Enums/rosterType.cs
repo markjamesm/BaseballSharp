3ca07a8d36226a4933a4a6d5e1b29694fb9202c0
Mark-James McDougall <markjames@hh-dev.com>
2021-08-07 10:02:59 -0400

    - Updating documentation

--- src/Enums/rosterType.cs
+++ src/Enums/rosterType.cs
@@ -2,8 +2,19 @@
 {
     public enum rosterType
     {
+        /// <summary>
+        /// The full roster.
+        /// </summary>
         rosterFull,
+
+        /// <summary>
+        /// The 25-man roster.
+        /// </summary>
         roster25,
+
+        /// <summary>
+        /// The 40-man roster.
+        /// </summary>
         roster40
     }
 }
